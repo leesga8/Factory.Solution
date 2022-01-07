30dd5ea32c795a5c482b230cd493fd039ab7d8a8
Albert Lee <leesga8@gmail.com>
2022-01-07 13:43:08 -0800

    add models

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System; 
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+    public class Engineer
+    {
+        public Engineer()
+        {
+            this.JoinEntities = new HashSet<EngineerMachine>();
+        }
+
+        public int EngineerId { get; set; }
+        public string Name { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime LicenseDate { get; set; }
+        public bool Repairs { get; set; }
+
+        public virtual ICollection<EngineerMachine> JoinEntities { get;}
+    }
+}
---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System; 
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public string Name{ get; set; }
+    public int MachineId { get; set; }    
+    [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime InspectionDate { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set ;}
+  }
+}
