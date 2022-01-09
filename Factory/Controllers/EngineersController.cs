f9eb6c5f6efbe212b4d197280e3fc3d066a31cd9
Albert Lee <leesga8@gmail.com>
2022-01-08 23:16:45 -0800

    add route to Remove a Join Entity

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -101,5 +101,15 @@ namespace Factory
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+
+    [HttpPost]
+    public ActionResult DeleteMachine(int joinId)
+    {
+      var joinEntry = _db.EngineerMachine.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
+      _db.EngineerMachine.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
