93ee5d67ac88dc94fa881cca393e6717a83d4dbd
Albert Lee <leesga8@gmail.com>
2022-01-08 22:42:45 -0800

    add Machine UPDATE, views and getpost routes

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -43,6 +43,19 @@ namespace Factory
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult Edit(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Machine machine)
+    {
+      _db.Entry(machine).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
 
   }
